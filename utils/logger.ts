64369a3fe50a890ee260b7d8e059877680f042eb
Wilson <wilsonxin@hotmail.com>
2021-02-18 13:30:41 +0800

    Removed unnecessary code.

--- utils/logger.ts
+++
@@ -1,7 +0,0 @@
-import winston from "winston";
-//process environment for PROD would be set to error, this is shortcut for demo
-
-export const logger = winston.createLogger({
-  level: process.env.LOG_LEVEL,
-  transports: [new winston.transports.Console()],
-});
